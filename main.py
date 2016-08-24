bd641dc106a9642b56f7b4751caa6b494ac1220d
Matt Hall <matt@agilegeoscience.com>
2016-08-23 21:11:51 -0300

    adding files

---
+++ main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Simple application to get data from images.
+
+by Matt Hall, Agile Geoscience, 2016
+"""
+from io import BytesIO
+import base64
+
+from flask import Flask
+from flask import make_response
+from flask import request, jsonify, render_template
+
+from PIL import Image
+
+from errors import InvalidUsage
+
+application = Flask(__name__)
+
+
+@application.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+#
+# Seismic frequency and SEGY bot
+#
+@application.route('/rainbow')
+def rainbow():
+    # Params from inputs.
+    url = request.args.get('url')
+    pass
+
+
+@application.route('/')
+def main():
+    return render_template('index.html', title='Home')
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
